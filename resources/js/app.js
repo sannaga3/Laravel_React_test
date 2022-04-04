1f537da55c93ce9a75e5afd961686ecc7cb6a955
sannaga3 <n.tetsuya02@gmail.com>
2022-04-04 23:36:43 +0900

    ルーティング設定

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,24 @@
-require('./bootstrap');
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Example from './components/Example'
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
+const App = () => {
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+            </Switch>
+        </BrowserRouter>
 
-if (document.getElementById('app')) {
-    ReactDOM.render(<Example />, document.getElementById('app'));
+    );
+};
+
+if (document.getElementById("app")) {
+    ReactDOM.render(<App />, document.getElementById("app"));
 }
